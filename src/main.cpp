85c1e86dc10193c78dcd863f7479cf4ba824eed7
Головин <dima.golovn2005@mail.ru>
2026-08-18 00:13:47 +0300

    Fix: make windows.h for cross-platform build

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 ﻿#include <QApplication>
 #include "simulation_window.h"
+
+#ifdef _WIN32
 #include <windows.h>
+#endif
 
 int main(int argc, char *argv[])
 {
+#ifdef _WIN32
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
+#endif
 
     QApplication app(argc, argv);
     SimulationWindow window;
